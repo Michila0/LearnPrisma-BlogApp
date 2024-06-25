083f87030ad9ddc9c6f07a60fc288c7b87b56e14
Michila0 <michila.bimsara12@gmail.com>
2024-06-25 19:58:24 +0530

    feat: create the blogs page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import FormNewPost from "@/components/form-new-post";
+import BlogsPage from "@/app/blogs/page";
 
 export default function Home() {
   return (
     <main className='max-w-4xl mx-auto my-5'>
-      <FormNewPost/>
+        <FormNewPost/>
     </main>
   );
 }
