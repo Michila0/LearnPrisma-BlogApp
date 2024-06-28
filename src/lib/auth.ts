2c3b594ed3a8b4aa7eca1925a79a6e1d1deed2ed
Michila0 <michila.bimsara12@gmail.com>
2024-06-28 13:24:59 +0530

    bug: google authentication part

---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import 'dotenv/config';
+// require('dotenv').config();
+
+import prisma from "@/lib/db";
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+    ],
+}
